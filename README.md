abfe3575abb56e1165e344a7b9009f4ef6b18a36
Christopher Zenzel <chriszenzel@outlook.com>
2015-06-05 20:05:57 -0400

    Initial Source Code for Bookmarklet
    Internet Explorer Bookmarklet for Testing in GH Pages.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# OutlookFWReply
-Outlook "Forward as Attachment" - Online Reply
+# Reply to "Forward as Attachment" and "Attached Forwarded Message" on Outlook.com
+
+Reply to attached forwared messages on Outlook.com using Internet Explorer, Chrome, Firefox, or Safari by using bookmarklets or native extensions. This plugin is useful when replying to messages that are forwarded automaticlly from another account in Outlook or Exchange Server as attachments to avoid problems with DMARC, SPF, or other policies blocking direct re-directs of messages to Outlook.com.
+
+## The Team
+
+This plugin was made possible by:
+
+* Christopher Zenzel, AA, IST
+
+## License and Copyright
+
+Copyright 2015 (c) Christopher Zenzel, AA, IST
+
+This plugin is currently open source, but you may only use it for personal, non-commerical use. If you want to integrate or use this plugin for other reasons please let me know via Twitter or Facebook. You may modify the source of this plugin or extension, but I ask that you share the source code back to my GitHub source through the Wiki, Issues, or Branching.
---
+++ scripts/ofr-bookmark.js
@@ -0,0 +1,93 @@
+/*
+ * Outlook.com Forward as Attachment Reply for Internet Explorer
+ * This is only for Internet Explorer and Bookmark Based Initialization
+ * Copyright 2015 (c) Christopher Zenzel. All Rights Reserved.
+ * For information on using or integrating this script contact @czenzel on Twitter.
+ */
+
+$(document).ready(function() {
+	_z_rta_waitForMessageView("#ComposeRteEditor_surface", 1250);
+});
+
+function _z_rta_ComposeViewLink(myAddrs) {
+	var myAddrsLink = '$($("#toCP").find("textarea")[1]).val("' + myAddrs + '");';
+
+	$(".ComposeHeader").append("<p id='zRtaReply'><a onclick='" + myAddrsLink + "'><strong>Reply</strong> to Forwarded Attachment</a></p>");
+
+	if (myAddrs == '') $("#zRtaReply").remove();
+}
+
+function _z_rta_ComposeRAViewLink(myAddrs) {
+	var myAddrsLink = '$($("#toCP").find("textarea")[1]).val("' + myAddrs + '");';
+
+	$(".ComposeHeader").append("<p id='zRtaReplyAll'><a onclick='" + myAddrsLink + "'><strong>Reply All</strong> to Forwarded Attachment</a></p>");
+
+	if (myAddrs == '') $("#zRtaReplyAll").remove();
+}
+
+function _z_rta_ReplyAllToAttachment(myTextHtml) {
+	var myAddrs = '';
+	var myFromRegex = /--Forwarded Message Attachment--\<br\>From\:\s*(\w*\@\w*\.\w*)\<br\>/;
+	var myCcRegex = /--Forwarded Message Attachment--.*Cc\:\s*([\w*\@\w*\.\w*\,?\s*?]*)\<br\>/;
+	var myMatch = myFromRegex.exec(myTextHtml);
+	if (myMatch != null) { myAddrs += myMatch[1] + ','; };
+	myMatch = myCcRegex.exec(myTextHtml);
+	if (myMatch != null) { myAddrs += myMatch[1] + ','; };
+	_z_rta_waitForComposeRAView('.ComposeHeader', myAddrs, 1250);
+}
+
+function _z_rta_ReplyToAttachment(myTextHtml) {
+	var myAddrs = '';
+	var myFromRegex = /--Forwarded Message Attachment--\<br\>From\:\s*(\w*\@\w*\.\w*)\<br\>/;
+	var myMatch = myFromRegex.exec(myTextHtml);
+	if (myMatch != null) { myAddrs += myMatch[1] + ','; };
+	_z_rta_waitForComposeView('.ComposeHeader', myAddrs, 1250);
+}
+
+function _z_rta_waitForComposeView(selector, attr, time) {
+        if(document.querySelector(selector) != null) {
+		_z_rta_ComposeViewLink(attr);
+		return;
+	}
+	else {
+		setTimeout(function() {
+			_z_rta_waitForComposeView(selector, attr, time);
+		}, time);
+	}
+}
+
+function _z_rta_waitForComposeRAView(selector, attr, time) {
+        if(document.querySelector(selector) != null) {
+		_z_rta_ComposeRAViewLink(attr);
+		return;
+	}
+	else {
+		setTimeout(function() {
+			_z_rta_waitForComposeRAView(selector, attr, time);
+		}, time);
+	}
+}
+
+function _z_rta_waitForMessageView(selector, time) {
+        if(document.querySelector(selector) != null) {
+		var flagBind = false;
+		$("#ComposeRteEditor_surface").contents().find("body").bind('DOMSubtreeModified', function() {
+			var contents = $($("#ComposeRteEditor_surface").contents().find("body")).html();
+			_z_rta_ReplyToAttachment(contents);
+			_z_rta_ReplyAllToAttachment(contents);
+			$("#ComposeRteEditor_surface").contents().find("body").unbind('DOMSubtreeModified');
+			flagBind = true;
+		});
+		if (!flagBind) {
+			var contents = $($("#ComposeRteEditor_surface").contents().find("body")).html();
+			_z_rta_ReplyToAttachment(contents);
+			_z_rta_ReplyAllToAttachment(contents);
+		}
+		return;
+	}
+	else {
+		setTimeout(function() {
+			_z_rta_waitForMessageView(selector, time);
+		}, time);
+	}
+}
